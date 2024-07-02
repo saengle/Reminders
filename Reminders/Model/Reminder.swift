b2dc6a5fbf06d1507556362e3d81b1fcac9bc518
saengle <zohv817@gmail.com>
2024-07-02 23:48:32 +0900

    [Feat] Remainder Model

---
+++ Reminders/Model/Reminder.swift
@@ -0,0 +1,30 @@
+//
+//  Reminder.swift
+//  Reminders
+//
+//  Created by 쌩 on 7/2/24.
+//
+
+import Foundation
+
+import RealmSwift
+
+class Reminder: Object {
+    @Persisted(primaryKey: true) var id: ObjectId
+    @Persisted(indexed: true) var title: String = ""
+    @Persisted var priority: Int = 0
+    @Persisted var tag: String? = ""
+    @Persisted var content: String? = ""
+    @Persisted var deadLine = Date()
+    @Persisted var imagePath: String?
+    
+    convenience init(title: String ,priority: Int, content: String?, tag: String ,deadLine: Date, imagePath: String) {
+        self.init()
+        self.title = title
+        self.priority = priority
+        self.tag = tag
+        self.content = content
+        self.deadLine = deadLine
+        self.imagePath = imagePath
+    }
+}
