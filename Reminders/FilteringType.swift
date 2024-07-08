1b0a9c0a2c6214e40fb6d5c39f4cb5eb5cc6267c
saengle <zohv817@gmail.com>
2024-07-09 01:51:13 +0900

    [Add] enum FilteringType

---
+++ Reminders/FilteringType.swift
@@ -0,0 +1,17 @@
+//
+//  FilteringType.swift
+//  Reminders
+//
+//  Created by 쌩 on 7/9/24.
+//
+
+import Foundation
+
+enum FilteringType {
+    case all
+    case today
+    case scheduled
+    case flagged
+    case whole
+    case completed
+}
